b5ee52b0bac21087b25c69cd4d7cfd2e44dd2eba
Karamveer200 <stuntmania101@gmail.com>
2023-12-14 19:45:41 -0800

    Added chunk manager and simulated file storage

---
+++ contracts/_ChunkManager.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract ChunkManager {
+   function chunkContent(string memory content) internal pure returns (string[] memory) {
+        uint256 chunkSize = 32;
+        uint256 numChunks = (bytes(content).length + chunkSize - 1) / chunkSize;
+        string[] memory chunks = new string[](numChunks);
+
+        for (uint256 i = 0; i < numChunks; i++) {
+            uint256 start = i * chunkSize;
+            uint256 end = start + chunkSize;
+            if (end > bytes(content).length) {
+                end = bytes(content).length;
+            }
+            bytes memory chunkBytes = new bytes(end - start);
+            for (uint256 j = start; j < end; j++) {
+                chunkBytes[j - start] = bytes(content)[j];
+            }
+            chunks[i] = string(chunkBytes);
+        }
+
+        return chunks;
+    }
+
+    function concatenateChunks(string[] memory chunks) internal pure returns (string memory) {
+        uint256 totalLength = 0;
+        for (uint256 i = 0; i < chunks.length; i++) {
+            totalLength += bytes(chunks[i]).length;
+        }
+
+        bytes memory result = new bytes(totalLength);
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < chunks.length; i++) {
+            bytes memory chunkBytes = bytes(chunks[i]);
+            for (uint256 j = 0; j < chunkBytes.length; j++) {
+                result[currentIndex++] = chunkBytes[j];
+            }
+        }
+
+        return string(result);
+    }
+}
--- contracts/_FileStorage.sol
+++ contracts/_FileStorage.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-contract FileStorageManager {
+import "hardhat/console.sol";
+import "./_ChunkManager.sol";
+
+contract FileStorageManager is ChunkManager {
     address public owner;
     uint256 public availableStorage;
 
-    mapping(string => File) private fileIdToFile; // Mapping from fileId to File
-    mapping(string => address) private fileIdToUserId; // Mapping from fileId to userId
+    // Mapping from fileId to FileMetadata  
+    mapping(string => FileMetadata) private fileIdToMetadata;
 
-    event FileStored(string fileName, string fileId, uint256 newAvailableStorage); // need to return the same file id
-    event FileRetrieved(string fileName, string content);// need to return the same file id
-    event FileDeleted(string fileId,uint256 newAvailableStorage);
+    // Mapping from fileId to storage node
+    mapping(string => address[]) private fileIdToNode;
 
-    struct File { // Struct to store file details
-        string content;
+    // Mapping from fileId to chunks
+    mapping(string => string[]) private fileIdToChunks; 
+
+    struct FileMetadata {
         string fileName;
+        uint256 fileSize;
     }
 
     modifier onlyOwner() {
@@ -27,36 +32,71 @@ contract FileStorageManager {
         availableStorage = initialStorage;
     }
 
-    function storeFile(string memory fileName, string memory content, string memory uniqueId)  public {// one more argument 
+    function storeFile(string memory fileName, string memory content, string memory uniqueId, address[] memory storageNodes) public {
         uint256 fileSize = bytes(content).length;
+
         require(availableStorage >= fileSize, "No storage available");
+        require(storageNodes.length > 0, "At least one storage node is required");
+
         
-        fileIdToFile[uniqueId] = File(content, fileName);
-        fileIdToUserId[uniqueId] = msg.sender;
+        fileIdToMetadata[uniqueId] = FileMetadata(fileName, fileSize);
 
+        // Chunk the content and store the chunk IDs
+        string[] memory chunks = chunkContent(content);
+        fileIdToChunks[uniqueId] = chunks;
+
+        // Calculate how many nodes each chunk should be stored in
+        uint256 nodesPerChunk = storageNodes.length / chunks.length;
+        uint256 remainingNodes = storageNodes.length % chunks.length;
+
+        uint256 currentIndex = 0;
+
+        // Iterate through each chunk and distribute them to nodes
+        for (uint256 i = 0; i < chunks.length; i++) {
+            uint256 nodesToUse = nodesPerChunk;
+            
+            // If there are remaining nodes, use one extra node for each remaining chunk
+            if (remainingNodes > 0) {
+                nodesToUse++;
+                remainingNodes--;
+            }
+
+            // Store the chunk in the selected nodes
+            for (uint256 j = 0; j < nodesToUse; j++) {
+                // Logic for actually storing chunk in the node
+                currentIndex++;
+            }
+        }
         availableStorage -= fileSize;
-
-        emit FileStored(fileName, uniqueId, availableStorage);// pass back the file id from aruments
     }
-
 
     function retrieveFile(string memory fileId) public view returns (string memory) {
-        require(bytes(fileIdToFile[fileId].fileName).length > 0, "File not found");
-        return fileIdToFile[fileId].content;
+        require(fileIdToMetadata[fileId].fileSize > 0, "File not found");
+
+        // Retrieve chunks from storage nodes
+        string[] memory chunks = fileIdToChunks[fileId];
+        require(chunks.length > 0, "No chunks found for the file");
+
+        // Concatenate chunks into a single string
+        string memory concatenatedContent = concatenateChunks(chunks);
+
+        return concatenatedContent;
     }
 
-
     function deleteFile(string memory fileId) public {
-        require(bytes(fileIdToFile[fileId].fileName).length > 0, "File not found");
-        require(fileIdToUserId[fileId] == msg.sender || msg.sender == owner, "Unauthorized");
+        require(fileIdToMetadata[fileId].fileSize > 0, "File not found");
+        require(msg.sender == owner, "Unauthorized");
 
-        uint256 fileSize = bytes(fileIdToFile[fileId].content).length;
+        uint256 fileSize = fileIdToMetadata[fileId].fileSize;
         availableStorage += fileSize;
 
-        delete fileIdToFile[fileId];
-        delete fileIdToUserId[fileId];
+        // Also call File deletion in the node itself
 
-        emit FileDeleted(fileId, availableStorage);
-}
+        // Delete file metadata, node mapping, and chunks
+        delete fileIdToMetadata[fileId];
+        delete fileIdToNode[fileId];
+        delete fileIdToChunks[fileId];
 
+
+    }
 }
