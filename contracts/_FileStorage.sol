b165bc489a986d60ada658e3fc04b3fc639dea3c
Karamveer200 <stuntmania101@gmail.com>
2024-01-12 01:57:49 -0800

    Added NodeManager and handle chunk storage into the Node. Also handled available size logic

--- contracts/_FileStorage.sol
+++ contracts/_FileStorage.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "./_ChunkManager.sol";
+import "./_NodeManager.sol";
 
-contract FileStorageManager is ChunkManager {
+contract FileStorageManager is ChunkManager, NodeManager {
     address public owner;
-    uint256 public availableStorage;
 
     // Mapping from fileId to FileMetadata  
     mapping(string => FileMetadata) private fileIdToMetadata;
@@ -27,47 +26,32 @@ contract FileStorageManager is ChunkManager {
         _;
     }
 
-    constructor(uint256 initialStorage) {
+    constructor() NodeManager() {
         owner = msg.sender;
-        availableStorage = initialStorage;
     }
 
-    function storeFile(string memory fileName, string memory content, string memory uniqueId, address[] memory storageNodes) public {
+    function storeFile(string memory fileName, string memory content, string memory uniqueId) public {
         uint256 fileSize = bytes(content).length;
 
-        require(availableStorage >= fileSize, "No storage available");
-        require(storageNodes.length > 0, "At least one storage node is required");
-
-        
         fileIdToMetadata[uniqueId] = FileMetadata(fileName, fileSize);
 
         // Chunk the content and store the chunk IDs
         string[] memory chunks = chunkContent(content);
         fileIdToChunks[uniqueId] = chunks;
 
-        // Calculate how many nodes each chunk should be stored in
-        uint256 nodesPerChunk = storageNodes.length / chunks.length;
-        uint256 remainingNodes = storageNodes.length % chunks.length;
-
-        uint256 currentIndex = 0;
-
         // Iterate through each chunk and distribute them to nodes
         for (uint256 i = 0; i < chunks.length; i++) {
-            uint256 nodesToUse = nodesPerChunk;
-            
-            // If there are remaining nodes, use one extra node for each remaining chunk
-            if (remainingNodes > 0) {
-                nodesToUse++;
-                remainingNodes--;
-            }
+            uint256 chunkSize = bytes(chunks[i]).length;
+            address selectedNodeAddress = findAvailableNode(chunkSize);
 
-            // Store the chunk in the selected nodes
-            for (uint256 j = 0; j < nodesToUse; j++) {
-                // Logic for actually storing chunk in the node
-                currentIndex++;
-            }
+            require(selectedNodeAddress != address(0), "No available nodes");
+            fileIdToNode[uniqueId].push(selectedNodeAddress);
+
+            storeChunkInNode(selectedNodeAddress, chunks[i]);
+
+            // Update available storage of the current node
+            updateAvailableStorage(selectedNodeAddress, nodes[selectedNodeAddress].availableStorage - chunkSize);
         }
-        availableStorage -= fileSize;
     }
 
     function retrieveFile(string memory fileId) public view returns (string memory) {
@@ -87,16 +71,11 @@ contract FileStorageManager is ChunkManager {
         require(fileIdToMetadata[fileId].fileSize > 0, "File not found");
         require(msg.sender == owner, "Unauthorized");
 
-        uint256 fileSize = fileIdToMetadata[fileId].fileSize;
-        availableStorage += fileSize;
-
         // Also call File deletion in the node itself
 
         // Delete file metadata, node mapping, and chunks
         delete fileIdToMetadata[fileId];
         delete fileIdToNode[fileId];
         delete fileIdToChunks[fileId];
-
-
     }
 }
---
+++ contracts/_NodeManager.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract NodeManager {
+    struct Node {
+        address nodeAddress;
+        uint256 availableStorage;
+    }
+
+    mapping(address => Node) public nodes;
+    address[] public allNodes;
+
+    function addNode(address _nodeAddress, uint256 _initialStorage) public {
+        require(nodes[_nodeAddress].nodeAddress == address(0), "Node already exists");
+        nodes[_nodeAddress] = Node(_nodeAddress, _initialStorage);
+        allNodes.push(_nodeAddress);
+    }
+
+    function updateAvailableStorage(address _nodeAddress, uint256 _newStorage) public {
+        require(nodes[_nodeAddress].nodeAddress != address(0), "Node does not exist");
+        nodes[_nodeAddress].availableStorage = _newStorage;
+    }
+
+    function storeChunkInNode(address _nodeAddress, string memory _chunk) public {
+        // TODO: Store the chunk in Node
+    }
+
+
+    function getAllNodes() public view returns (address[] memory) {
+        return allNodes;
+    }
+
+    
+    function findAvailableNode(uint256 _chunkSize) internal view returns (address) {
+        uint256 numNodes = allNodes.length;
+        uint256 sizeOffset = 50;
+
+        if (numNodes == 0) {
+            return address(0);
+        }
+
+        // Generate a pseudo-random index using blockhash
+        uint256 randomIndex = uint256(blockhash(block.number - 1)) % numNodes;
+
+        // Iterate through the nodes starting from the pseudo-random index
+        for (uint256 i = 0; i < numNodes; i++) {
+            address node = allNodes[(randomIndex + i) % numNodes];
+            if (nodes[node].availableStorage > _chunkSize + sizeOffset) {
+                // Randomly return the first node found with available storage
+                return node;
+            }
+        }
+        // No available nodes
+        return address(0); 
+    }
+}
